c05001a2ba6a79622c0c12ea3de5d41948ea53be
Dileepa <dileepaniroshan@live.com>
2020-01-23 01:00:12 +0530

    added not found page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
+import Home from './components/pages/Home';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 
 import GithubState from './context/gituhub/GithubState';
 import AlertState from './context/alert/AlertState';
@@ -27,17 +27,14 @@ const App = () =>{
           <Alert/>
 
         <Switch>
-          <Route exact path ='/' render= { props => (
-            <Fragment>
-                  <Search  />
 
-              <Users/>
-            </Fragment>
-          )}/>
+        <Route exact path='/' component={Home} />
 
         <Route exact path='/about' component={About} />
 
         <Route exacr path='/user/:login' component={User} />
+
+        <Route component = {NotFound} />
 
         </Switch>
 
